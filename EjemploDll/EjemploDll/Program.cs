ccca994272760a749cc93869ac9638db72ec82ef
Andres <andres.jaag@hotmail.es>
2022-04-20 11:42:18 -0500

    Aplicacion de ordenar numeros

---
+++ EjemploDll/EjemploDll/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrdenarNumeros;
+
+namespace EjemploDll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OrdenadorNumerico objOrdenador=new OrdenadorNumerico();
+            List<int> listaDesordenada = new List<int>();
+            listaDesordenada.Add(1);
+            listaDesordenada.Add(3);
+            listaDesordenada.Add(5);
+            listaDesordenada.Add(7);
+            listaDesordenada.Add(2);
+            listaDesordenada.Add(4);
+            listaDesordenada.Add(6);
+            Console.WriteLine("Lista desordenada:");
+            Console.WriteLine();
+            for(int i = 0; i < listaDesordenada.Count; i++)
+            {
+                Console.WriteLine(listaDesordenada[i].ToString());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Lista ordenada desendente:");
+            Console.WriteLine();
+
+            listaDesordenada=objOrdenador.ordenarDescendente(listaDesordenada);
+
+            for (int i = 0; i < listaDesordenada.Count; i++)
+            {
+                Console.WriteLine(listaDesordenada[i].ToString());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Lista ordenada ascendente:");
+            Console.WriteLine();
+
+            listaDesordenada = objOrdenador.ordenarAscendente(listaDesordenada);
+
+            for (int i = 0; i < listaDesordenada.Count; i++)
+            {
+                Console.WriteLine(listaDesordenada[i].ToString());
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ OrdenarNumeros/OrdenarNumeros/OrdenadorNumerico.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace OrdenarNumeros
+{
+    public class OrdenadorNumerico
+    {
+        public List<int> ordenarDescendente(List<int> listaNumeros)
+        {
+            List<int> ordenados = listaNumeros;
+            ordenados.Sort((a, b) =>b.CompareTo(a));
+            return ordenados;
+        }
+        public List<int> ordenarAscendente(List<int> listaNumeros)
+        {
+            List<int> ordenados = listaNumeros;
+            ordenados.Sort((a, b) => a.CompareTo(b));
+            return ordenados;
+        }
+    }
+}
